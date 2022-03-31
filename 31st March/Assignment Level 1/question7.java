e15f0bc74f54ebee005da5706d8576546cfdbb2a
NancyTindwani <100419480+NancyTindwani@users.noreply.github.com>
2022-03-31 22:26:49 +0530

    Update question7.java

--- 31st March/Assignment Level 1/question7.java
+++ 31st March/Assignment Level 1/question7.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class question7 {
     public static void main(String args[])
     {
-        int x, y, z;
+        int x, y, z,l;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the first number:");
         x = s.nextInt();
@@ -16,12 +16,31 @@ public class question7 {
         }
         else if(y > z)
         {
+
             System.out.println("Largest number is:"+y);
         }
         else
         {
             System.out.println("Largest number is:"+z);
         }
+        if(x>y && x>z)
+        {
+            if(y>z)
+                System.out.print("2nd largest: "+y);
+            else
+                System.out.print("2nd largest: "+z);
+        }
+        else if(y>z && y>x)
+        {
+            if(z>x)
+                System.out.print("2nd largest: "+z);
+            else
+                System.out.print("2nd largest: "+x);
+        }
+        else if(x>y)
+            System.out.print("2nd largest: "+x);
+        else
+            System.out.print("2nd largest: "+y);
 
     }
 }
