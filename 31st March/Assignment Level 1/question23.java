539f1aa8f9c94e5caf57cead9cf51b9885b4815f
NancyTindwani <100419480+NancyTindwani@users.noreply.github.com>
2022-03-31 22:28:49 +0530

    Update question23.java

--- 31st March/Assignment Level 1/question23.java
+++ 31st March/Assignment Level 1/question23.java
@@ -1,6 +1,22 @@
+import static java.lang.Math.pow;
+import java.util.Scanner;
 public class question23 {
     public static void main(String args[])
     {
-
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter the range: ");
+                int num = sc.nextInt();
+                int even = 1, oddsum = 1;
+                for(int i = 0; i <= num; i++)
+                {
+                    if(i%2!=0){
+                        System.out.print(even*(-2)+" ");
+                        even += 2;
+                    }
+                    else{
+                        System.out.print(oddsum+" ");
+                        oddsum += 3;
+                    }
+                }
     }
 }
